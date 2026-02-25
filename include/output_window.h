6e25ce9a51c2fb6beaa00bdf1da97fa71ef395cb
Dev-Tade <88458312+Dev-Tade@users.noreply.github.com>
2026-02-25 16:06:34 -0300

    Output window from layout with naive text rendering

---
+++ include/output_window.h
@@ -0,0 +1,33 @@
+#ifndef OUTPUT_WINDOW_H
+#define OUTPUT_WINDOW_H
+
+#include "raygui.h"
+#include "rglp.h"
+
+typedef struct OutputWindow
+{
+  RGLayout layout;
+
+  // Windows status bar
+  Rectangle dragHandle;
+
+  RGLControl *window;
+  RGLAnchor *anchor;
+
+  // Scroll panel state
+  Rectangle scrollContent;
+  Rectangle scrollView;
+  Vector2 scrollOffset;
+
+
+  // Window state
+  bool windowActive;
+
+  char *outputText;
+
+} OutputWindow;
+
+void OutputWindowInit(OutputWindow *output, const char *outputWindowLayoutFile);
+void GuiOutputWindow(OutputWindow *state);
+
+#endif //!OUTPUT_WINDOW_H
---
+++ src/output_window.c
@@ -0,0 +1,70 @@
+#include "output_window.h"
+
+void OutputWindowInit(OutputWindow *output, const char *outputWindowLayoutFile)
+{
+  if (output == NULL) return;
+
+  output->layout = LoadLayoutFile(outputWindowLayoutFile);
+
+  // Expose window and anchor
+  RGLControl *window = GetControlByName(&output->layout, "OutputWindow");
+  output->anchor = GetAnchorById(&output->layout, window->anchorID);
+  output->window = window;
+
+  // Expose window drag handle (status bar)
+  output->dragHandle = GetControlRect(&output->layout, window);
+  output->dragHandle.height = 24; // raygui window statusbar height 
+
+  // Default state
+  output->windowActive = true;
+}
+
+void GuiOutputWindow(OutputWindow *output)
+{
+  int screenWidth = GetScreenWidth();
+  int screenHeight = GetScreenHeight();
+
+  // Move window down and expand to full width
+  RGLControl *window = GetControlByName(&output->layout, "OutputWindow");
+  RGLAnchor *anchor = GetAnchorById(&output->layout, window->anchorID);
+
+  anchor->pos.y = screenHeight - window->rect.height;
+  window->rect.width = screenWidth;
+
+  // Update drag handle
+  Rectangle windowRect = GetControlRect(&output->layout, window);
+  output->dragHandle = windowRect;
+  output->dragHandle.height = 24; // raygui window status bar
+
+  if (output->windowActive)
+  {
+    output->windowActive = !GuiWindowBox(windowRect, window->text);
+
+    // Update scroll panel to be inside window
+    RGLControl *scrollPanel = GetControlByName(&output->layout, "OutputTextScroll");
+    scrollPanel->rect.width = screenWidth;
+    scrollPanel->rect.height = GetControlRect(&output->layout, window).height;
+
+    output->scrollContent = GetControlRect(&output->layout, scrollPanel);
+    output->scrollContent.height = 1000;
+
+    GuiScrollPanel(GetControlRect(&output->layout, scrollPanel), NULL, output->scrollContent, &output->scrollOffset, &output->scrollView);
+
+      BeginScissorMode(output->scrollView.x, output->scrollView.y, output->scrollView.width, output->scrollView.height);
+
+      Rectangle text_rect = {output->scrollContent.x + output->scrollOffset.x, output->scrollContent.y + output->scrollOffset.y, output->scrollContent.width, output->scrollContent.height};
+
+      int prevTextAlignment = GuiGetStyle(DEFAULT, TEXT_ALIGNMENT_VERTICAL);
+      int prevTextWrap = GuiGetStyle(DEFAULT, TEXT_WRAP_MODE);
+      
+      GuiSetStyle(DEFAULT, TEXT_ALIGNMENT_VERTICAL, TEXT_ALIGN_TOP);
+      GuiSetStyle(DEFAULT, TEXT_WRAP_MODE, TEXT_WRAP_WORD); // TEXT_WRAP_CHAR
+      
+      GuiLabel(text_rect, output->outputText);
+      
+      GuiSetStyle(DEFAULT, TEXT_WRAP_MODE, prevTextWrap);
+      GuiSetStyle(DEFAULT, TEXT_ALIGNMENT_VERTICAL, prevTextAlignment);
+
+      EndScissorMode();
+  }
+}
